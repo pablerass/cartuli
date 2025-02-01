4e7048d35e16422fad2b79b09abfb67f1b4c7f94
Pablo Muñoz <pablerass@gmail.com>
2025-02-01 23:55:11 +0100

    Add draw registration mark function

--- cartuli/output.py
+++ cartuli/output.py
@@ -1,11 +1,69 @@
 """Sheet module."""
 import logging
+import reportlab.graphics.shapes as shapes
 
+from functools import lru_cache, partial
+from math import sin, cos, radians
 from pathlib import Path
+from reportlab.lib.colors import transparent
 from reportlab.lib.utils import ImageReader
 from reportlab.pdfgen import canvas
 
+from .measure import Line, Point, Size, mm
 from .sheet import Sheet
+
+
+@lru_cache
+def _rotate(origin: Point, point: Point, degrees: float) -> Point:
+    angle = radians(degrees)
+    x = origin.x + cos(angle) * (point.x - origin.x) - sin(angle) * (point.y - origin.y)
+    y = origin.y + sin(angle) * (point.x - origin.x) + cos(angle) * (point.y - origin.y)
+    return Point(x, y)
+
+
+def draw_registration_mark(canvas: canvas.Canvas, position: Point, size: Size = Size(5*mm, 5*mm),
+                           strokeWidth: float = 0.5) -> None:
+    draw = shapes.Drawing(*size)
+
+    # Main cross
+    draw.add(shapes.Line(0, size.height/2, size.width, size.height/2, strokeWidth=strokeWidth))
+    draw.add(shapes.Line(size.width/2, 0, size.width/2, size.height, strokeWidth=strokeWidth))
+
+    # Main cross ends
+    cross_end_length = (size.height + size.width)/2/5
+    draw.add(shapes.Line(size.width/2-cross_end_length/2, size.height,
+                         size.width/2+cross_end_length/2, size.height, strokeWidth=strokeWidth))
+    draw.add(shapes.Line(size.width, size.height/2-cross_end_length/2,
+                         size.width, size.height/2+cross_end_length/2, strokeWidth=strokeWidth))
+
+    # Diagonal cross
+    circle_center = Point(size.height/2, size.width/2)
+    circle_radius = (size.width + size.height)/4
+    initial_point = Point(circle_radius, 0)
+    rotate = partial(_rotate, circle_center, initial_point)
+    draw.add(shapes.Line(rotate(45).x, rotate(45).y, rotate(225).x, rotate(225).y,
+                         strokeWidth=strokeWidth))
+    draw.add(shapes.Line(rotate(135).x, rotate(135).y, rotate(315).x, rotate(315).y,
+                         strokeWidth=strokeWidth))
+
+    # Outer circle
+    outer_circle_radius = circle_radius/(3/2)
+    draw.add(shapes.Circle(circle_center.x, circle_center.y, outer_circle_radius, fillColor=transparent,
+             strokeWidth=strokeWidth))
+
+    # Inner circle
+    inner_circle_radius = outer_circle_radius/(3/2)
+    draw.add(shapes.Circle(circle_center.x, circle_center.y, inner_circle_radius, fillColor=transparent,
+             strokeWidth=strokeWidth))
+    draw.add(shapes.Wedge(circle_center.x, circle_center.y, inner_circle_radius, 0, 90, strokeWidth=0))
+    draw.add(shapes.Wedge(circle_center.x, circle_center.y, inner_circle_radius, 180, 270, strokeWidth=0))
+
+    draw.drawOn(canvas, position.x - size.height/2, position.y - size.width/2)
+
+
+def draw_crop_mark(canvas: canvas.Canvas, line: Line, strokeWidth: float = 0.5) -> None:
+    canvas.setLineWidth(strokeWidth)
+    canvas.line(*list(line))
 
 
 def sheet_pdf_output(sheet: Sheet, output_path: Path | str) -> None:
@@ -16,9 +74,9 @@ def sheet_pdf_output(sheet: Sheet, output_path: Path | str) -> None:
     c = canvas.Canvas(str(output_path), pagesize=tuple(sheet.size))
     for page in range(1, sheet.pages + 1):
         # Front
+        draw_registration_mark(c, Point(10*mm, 10*mm))
         for line in sheet.crop_marks:
-            c.setLineWidth(0.5)
-            c.line(*list(line))
+            draw_crop_mark(c, line)
 
         for i, card in enumerate(sheet.page_cards(page)):
             num_card = i + 1
@@ -44,8 +102,7 @@ def sheet_pdf_output(sheet: Sheet, output_path: Path | str) -> None:
                             card.back.image_size.width, card.back.image_size.height)
 
         for line in sheet.crop_marks:
-            c.setLineWidth(0.5)
-            c.line(*list(line))
+            draw_crop_mark(c, line)
 
         c.showPage()
         logger.debug(f"Created {output_path} page {page}")
